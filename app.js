ba652a4fdd86673ee8ad070f21ae8aa062a51999
Priyanshu Kumar choubey <104070944+PriyanshuChoubey@users.noreply.github.com>
2024-07-16 00:46:07 +0530

    Project completed

--- app.js
+++ app.js
@@ -6,22 +6,20 @@ const path = require("path");
 
 const http = require("http");       //http server only support socket.io 
 const socketio = require("socket.io");
-const { log } = require("console");
 const server = http.createServer(app);  //method to create http server is createServer()
-
 const io = socketio(server);    //now server and socketio is connected
 
 //now lets set ejs
 app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public"))); //here static files are setup
+app.use(express.static(path.join(__dirname,"public"))); //here static files are setup
 
-io.on("connection",function(socket){    //this will handle the request make by io in frontend
+io.on("connection",function(socket){    //this will handle the request make by socket io from frontend
     socket.on("send-location", function(data){
-        io.emit("receive-location",{id:socket.id, ...data})
+        io.emit("receive-location",{id:socket.id, ...data});
     });
     socket.on("disconnect",function(){
         io.emit("user-disconnected",socket.id);
-    })
+    });
 });
 
 app.get("/",function(req,res){
--- public/js/script.js
+++ public/js/script.js
@@ -18,9 +18,9 @@ if(navigator.geolocation){  //checking wheather the browser support geolocation 
     );
 }
 
-const map = L.map("map").setView([0,0],10);
+const map = L.map("map").setView([0,0],16);
 
-L.tileLayer("https://{s}.tile.openStreetmap.org/{z}/{x}/{y}.png",{
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     attribution: "Priyanshu Choubey",
 }).addTo(map);
 
@@ -29,12 +29,18 @@ const markers = {};
 socket.on("receive-location",(data)=>{
     const {id,latitude,longitude} = data;
     map.setView([latitude, longitude]);
+
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+    map.setView([latitude, longitude]);
 });
 
 socket.on("user-disconnected",(id) => {
-    if(marker[id]){
+    if(markers[id]){
         map.removeLayer(markers[id]);
         delete markers[id];
-    }
-    
+    }   
 });
